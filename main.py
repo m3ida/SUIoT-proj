06bcb110bf7b43b216ed29437edfec1fdc1b25d2
m3ida <andre.almeida@mare.arditi.pt>
2025-05-13 14:18:53 +0100

    changes

--- main.py
+++ main.py
@@ -28,14 +28,14 @@ def get_distance():
 
     print(3)
 
-    pulse_start = 0
+    #pulse_start = 0
     # Wait for echo to go high
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
 
     print(4)
 
-    pulse_end = 0
+    #pulse_end = 0
     # Wait for echo to go low
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
