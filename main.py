af5eb203c1659feb165b4a506e261334d7394826
m3ida <andre.almeida@mare.arditi.pt>
2025-05-14 10:58:59 +0100

    change

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ list_license_plates= []
 cropped_licenses = []
 # model = YOLO("./license_detection.pt")
 # modelDigits = YOLO("./digit_detection_font4.pt")
-# modelClassifyDigits = YOLO("./digit_classification_X.pt")
+# # modelClassifyDigits = YOLO("./digit_classification_X.pt")
 # classification_model = tflite.Interpreter(model_path="model.tflite")
 # classification_model.allocate_tensors()
 # input_details = classification_model.get_input_details()
@@ -88,73 +88,73 @@ def resize_with_padding(image):
     return new_im
 
 # def analyse_image(image):
-    results = model([image])
-    if(len(results) == 0):
-        print("No License Plate found")
-    for id2,result in enumerate(results):
-        boxes = result.boxes
+    # results = model([image])
+    # if(len(results) == 0):
+    #     print("No License Plate found")
+    # for id2,result in enumerate(results):
+    #     boxes = result.boxes
 
-        if(len(boxes.xywh)>0):
-            xywh_cpu = boxes.xywh.to('cpu').numpy()
+    #     if(len(boxes.xywh)>0):
+    #         xywh_cpu = boxes.xywh.to('cpu').numpy()
 
-            x = xywh_cpu[0][0]
-            y = xywh_cpu[0][1]
-            w = xywh_cpu[0][2]
-            h = xywh_cpu[0][3]
+    #         x = xywh_cpu[0][0]
+    #         y = xywh_cpu[0][1]
+    #         w = xywh_cpu[0][2]
+    #         h = xywh_cpu[0][3]
 
-            license_plate_crop = result.orig_img[int(y-h/2-5):int(y+h/2+5), int(x-w/2-5):int(x+w/2+5)]
+    #         license_plate_crop = result.orig_img[int(y-h/2-5):int(y+h/2+5), int(x-w/2-5):int(x+w/2+5)]
 
-            result = model([license_plate_crop])
+    #         result = model([license_plate_crop])
 
-            resultsDigits = modelDigits([license_plate_crop])
+    #         resultsDigits = modelDigits([license_plate_crop])
 
-            license_lennet = ""
-            license_yolo = ""
+    #         license_lennet = ""
+    #         # license_yolo = ""
 
-            for id,r in enumerate(resultsDigits):
-                boxes = r.boxes
+    #         for id,r in enumerate(resultsDigits):
+    #             boxes = r.boxes
 
-                if(len(boxes.xywh)>0):
-                    xywh_cpu = boxes.xywh.to('cpu').numpy()
+    #             if(len(boxes.xywh)>0):
+    #                 xywh_cpu = boxes.xywh.to('cpu').numpy()
 
-                    print(xywh_cpu)
+    #                 print(xywh_cpu)
 
-                sorted_boxes = sorted(xywh_cpu, key=lambda x: x[0])
-
-
-                heights = [coords[3] for coords in sorted_boxes]
-                avg_height = np.mean(heights)
-
-                img_h, img_w = license_plate_crop.shape[:2]
-
-                filtered_boxes = [
-                coords for coords in sorted_boxes
-                if coords[3] > 0.5 * avg_height
-                and 0.3 < coords[1] / img_h < 0.7
-                and 0.05 < coords[0] / img_w < 0.85
-            ]
+    #             sorted_boxes = sorted(xywh_cpu, key=lambda x: x[0])
 
 
-                for coords in filtered_boxes:
-                    x = coords[0]
-                    y = coords[1]
-                    w = coords[2]
-                    h = coords[3]
+    #             heights = [coords[3] for coords in sorted_boxes]
+    #             avg_height = np.mean(heights)
 
-                    cropped_character = r.orig_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)]
+    #             img_h, img_w = license_plate_crop.shape[:2]
 
-                    class_res = modelClassifyDigits(cropped_character)
-                    print("Prediction YOLO", decoder[int(class_res[0].probs.top1)])
-                    license_yolo += decoder[int(class_res[0].probs.top1)]
+    #             filtered_boxes = [
+    #             coords for coords in sorted_boxes
+    #             if coords[3] > 0.5 * avg_height
+    #             and 0.3 < coords[1] / img_h < 0.7
+    #             and 0.05 < coords[0] / img_w < 0.85
+    #         ]
 
-                    prediction_lennet = classify_character_tflite(cropped_character)
-                    print("Prediction LENNET:", prediction_lennet)
-                    license_lennet += prediction_lennet
 
-            print("Prediction LENNET:", license_lennet)
-            print("Prediction YOLO:", license_yolo)
+    #             for coords in filtered_boxes:
+    #                 x = coords[0]
+    #                 y = coords[1]
+    #                 w = coords[2]
+    #                 h = coords[3]
 
-            debug_imshow("License Plate", license_plate_crop, waitKey=True)
+    #                 cropped_character = r.orig_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)]
+
+    #                 # class_res = modelClassifyDigits(cropped_character)
+    #                 # print("Prediction YOLO", decoder[int(class_res[0].probs.top1)])
+    #                 # license_yolo += decoder[int(class_res[0].probs.top1)]
+
+    #                 prediction_lennet = classify_character_tflite(cropped_character)
+    #                 print("Prediction LENNET:", prediction_lennet)
+    #                 license_lennet += prediction_lennet
+
+    #         print("Prediction LENNET:", license_lennet)
+    #         # print("Prediction YOLO:", license_yolo)
+
+    #         debug_imshow("License Plate", license_plate_crop, waitKey=True)
 
 def get_distance():
     # Ensure trigger is low
